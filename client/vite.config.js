3080cf8c7ab43a813128f55448c0d499a822b5df
lucas.goncalves <lucas.goncalves@govone.digital>
2024-12-23 12:31:12 -0300

    feat: configuração para acesso do vite.config e atualização da lista do gitignore

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,16 @@
+// import path from "path";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -8,5 +21,9 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: true, //Permite acesso externo
+    port: 5173, //Porta de acesso
   },
 });
